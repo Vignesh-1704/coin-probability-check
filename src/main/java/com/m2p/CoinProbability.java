f98dad9664b6823705a442b3b71f1d47c03ffeee
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-31 14:07:53 +0530

    Requirement 1 Add. Included The Logic And Test Cases for Checking The Equality Of Probabilities

--- src/main/java/com/m2p/CoinProbability.java
+++ src/main/java/com/m2p/CoinProbability.java
@@ -1,4 +1,25 @@
 package com.m2p;
 
 public class CoinProbability {
+    private final double tossProbability;
+
+    public CoinProbability(double tossProbability) {
+        this.tossProbability = tossProbability;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        if(this == object)
+        {
+            return true;
+        }
+        if(object == null || object.getClass() != getClass())
+        {
+            return false;
+        }
+        CoinProbability that = (CoinProbability) object;
+        return tossProbability == that.tossProbability;
+    }
+
 }
--- src/test/java/com/m2p/CoinProbabilityTest.java
+++ src/test/java/com/m2p/CoinProbabilityTest.java
@@ -1,5 +1,41 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class CoinProbabilityTest {
-    CoinProbability coinProbability = new CoinProbability();
+
+    @Nested
+    class Requirement1 {
+        @Test
+        void toCheckProbabilityOfHeadIsEqualToProbabilityOfTail() {
+            CoinProbability headsProbability = new CoinProbability(0.5);
+            CoinProbability tailsProbability = new CoinProbability(0.5);
+            assertThat(headsProbability, is(equalTo(tailsProbability)));
+        }
+
+        @Test
+        void toCheckProbabilityOfHeadIsEqualToProbabilityGetting1InADiceRoll() {
+            CoinProbability headsProbability = new CoinProbability(0.5);
+            CoinProbability tailsProbability = new CoinProbability(0.16);
+            assertThat(headsProbability, is(not(equalTo(tailsProbability))));
+        }
+
+        @Test
+        void toCheckProbabilityOfHeadIsEqualToNull() {
+            CoinProbability headsProbability = new CoinProbability(0.5);
+            assertThat(headsProbability, is(not(equalTo(null))));
+        }
+
+        @Test
+        void toCheckProbabilityOfHeadIsEqualToPrimitiveDataType() {
+            CoinProbability headsProbability = new CoinProbability(0.5);
+            double tailProbability = 0.5;
+            assertThat(headsProbability, is(not(equalTo(tailProbability))));
+        }
+    }
 }
